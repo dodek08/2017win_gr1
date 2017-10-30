dc57e5efc17e5ec564a1915fc3a26a3829253ee7
Dominik Kasperski <kasperski.dominik@gmail.com>
2017-10-30 15:39:08 +0100

    dealing with minor issues

--- kol1.py
+++ kol1.py
@@ -21,44 +21,56 @@ class Client:
 
 class Bank:
 	def __init__(self,name):
-        self.name=params
+		self.name=name
 		self.clientslist=[]
 		self.money_in=0
 		self.money_out=0
+
 	def add_client(self,newclient):
-		self.clientslist=[clientlist,newclient]
+		self.clientslist=self.clientslist.append(Client(newclient))
+
 	def cash_on_accounts(self):
 		sum=0
 		for i in range(len(self.clientslist)):
 			sum=sum+self.clientslist[i].account
 		return sum
 	def client_info(self):
-	if len(self.clientslist) == 0
-		print('there is no clients!')
-	else
-		for i in range(len(self.clientslist)):
+		if len(self.clientslist) == 0:
+			print('there is no clients!')
+			return 0
+		else:
+			for i in range(len(self.clientslist)):
 				print(self.clientslist[i].name,' owns $',self.clientslist[i].account)
+
 	def client_puts_money(self,params): #name amount
-		name=params[1]
-		amount=params[2]
+		name=params[0]
+		amount=params[1]
 		self.money_in=self.money_in+amount
 		for i in range(len(self.clientslist)):
-			if name == self.clientslist[i].name
+			if name == self.clientslist[i].name:
 				self.clientslist[i].account=self.clientslist[i].account+amount
-			else
+			else:
 				continue
+
 	def client_withdraws_money(self,params): #name amount
-		name=params[1]
-		amount=params[2]
+		name=params[0]
+		amount=params[1]
 		self.money_out=self.money_out+amount
 		for i in range(len(self.clientslist)):
-			if name == self.clientslist[i].name
+			if name == self.clientslist[i].name:
 				self.clientslist[i].account=self.clientslist[i].account-amount
-			else
+			else:
 				continue
+
 B=Bank('Test')
 B.add_client('d')
+B.add_client('e')
+B.client_info()
 B.client_puts_money(['d',100])
+B.cash_on_accounts()
 B.client_withdraws_money(['d',100])
-
-	
+# C=Client("C")
+# D=Client("D")
+# E=Client("E")
+# clientslist=[C, D, E]
+# print(clientslist[1].name)
