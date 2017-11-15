13556ceb5710290926a95771b596e85564e44981
Dominik  Kasperski <3kasperski@taurus.fis.agh.edu.pl>
2017-11-15 16:05:37 +0100

    task finished

--- kol1.py
+++ kol1.py
@@ -1,64 +1,99 @@
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can thing of any other features, you can add them.
-#This code shoud be runnable with 'python kol1.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#Do your best, show off with good, clean, well structured code - this is more important than number of features.
-#After you finish, be sure to UPLOAD this (add, commit, push) to the remote repository.
-#Good Luck
-
 #!#!usr/bin/env python2.7
 
+
+
+
+		
+
 class Client:
-	def __init__(self,params): #name
-		self.name=params
-		self.account=0
 
-class Bank:
-	def __init__(self,name):
-        self.name=params
-		self.clientslist=[]
-		self.money_in=0
-		self.money_out=0
-	def add_client(self,newclient):
-		self.clientslist=[clientlist,newclient]
-	def cash_on_accounts(self):
-		sum=0
-		for i in range(len(self.clientslist)):
-			sum=sum+self.clientslist[i].account
-		return sum
-	def client_info(self):
-	if len(self.clientslist) == 0
-		print('there is no clients!')
-	else
-		for i in range(len(self.clientslist)):
-				print(self.clientslist[i].name,' owns $',self.clientslist[i].account)
-	def client_puts_money(self,params): #name amount
-		name=params[1]
-		amount=params[2]
-		self.money_in=self.money_in+amount
-		for i in range(len(self.clientslist)):
-			if name == self.clientslist[i].name
-				self.clientslist[i].account=self.clientslist[i].account+amount
-			else
-				continue
-	def client_withdraws_money(self,params): #name amount
-		name=params[1]
-		amount=params[2]
-		self.money_out=self.money_out+amount
-		for i in range(len(self.clientslist)):
-			if name == self.clientslist[i].name
-				self.clientslist[i].account=self.clientslist[i].account-amount
-			else
-				continue
-B=Bank('Test')
-B.add_client('d')
-B.client_puts_money(['d',100])
-B.client_withdraws_money(['d',100])
 
-	
+	def __init__(self, name, id):
+		self.name = name
+		self.id = id
+		self.accounts = 0
+
+	def info(self):
+		return self.name, self.id, self.accounts
+
+class Bank():
+
+
+	def __init__(self, name, id):
+		self.name = name
+		self.id = id
+		self.clients_ids = []
+		self.clients_money = {} #client_id : amount
+		self.number_of_accounts_in_bank = 0
+
+	def add_client(self, client, input = 0):
+		self.clients_money[client.id] = input
+		client.accounts = input
+		self.clients_ids.append(client.id)
+		self.number_of_accounts_in_bank +=1
+
+	def put_money(self, client_id, amount):
+		self.clients_money[client_id] += amount
+
+	def withdraw_money(self, client_id, amount):
+		self.clients_money[client_id] -= amount
+
+
+class System():
+
+
+	def __init__(self):
+		self.banks = []
+		self.clients = []
+		self.number_of_banks = 0
+		self.number_of_clients = 0
+
+
+	def new_bank(self, name):
+		self.banks.append(Bank(name,self.number_of_banks))
+		self.number_of_banks += 1
+
+	def new_bank_client(self, clients_name, banks_name, bank_id):
+		self.clients.append(Client(clients_name,self.number_of_clients))
+		self.banks[bank_id].add_client(Client(clients_name,self.number_of_clients))
+		self.number_of_clients += 1
+
+	def put_money(self, client_id, amount):
+		bank_id = -1
+		for B in self.banks:
+			if B.clients_ids.count(client_id) > 0:
+				bank_id=B.id
+		if bank_id == -1:
+			raise ValueError("Client is not in any bank")
+		self.banks[bank_id].put_money(client_id, amount)
+		self.clients[client_id].accounts += amount
+
+	def withdraw_money(self, client_id, amount):
+		bank_id = -1
+		for B in self.banks:
+			if B.clients_ids.count(client_id) > 0:
+				bank_id=B.id
+		if bank_id == -1:
+			raise ValueError("Client is not in any bank")
+		self.banks[bank_id].withdraw_money(client_id, amount)
+		self.clients[client_id].accounts -= amount
+
+	def transfer_from_client_to_client(self, client_id_1, client_id_2, amount):
+		self.withdraw_money(client_id_1, amount)
+		self.put_money(client_id_2, amount)
+
+
+
+
+if __name__ == "__main__":
+
+	SYS = System()
+	SYS.new_bank("FISBANK")
+	SYS.new_bank("CERNBANK")
+	SYS.new_bank_client("Adam", "FISBANK", 0)
+	SYS.new_bank_client("Barbara", "CERNBANK", 1)
+	SYS.put_money(0,2000)
+	SYS.withdraw_money(0,100)
+	SYS.transfer_from_client_to_client(0,1,1000)
+	print(SYS.clients[0].info())
+	print(SYS.clients[1].info())
